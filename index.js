e935d3c63dfd8a2f9f87b7dcfe46140e57b9cd15
Manuel Zambrano <mmzmm3z@gmail.com>
2017-01-18 23:06:03 -0300

    Added suport for nicknames

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', socket => {
-	socket.on('chat message', msg => {
-		io.emit('chat message', msg);
-	});
-});
+    socket.on('chat message', msg => {
+        io.emit('chat message', msg);
+    });
+    
+    socket.on('disconnect', () => {
+        io.emit('user_disconnected');
+    });
 
-io.on('connection', socket => {
-	io.emit('user_conected');
-
-	socket.on('disconnect', () => {
-		io.emit('user_disconnected');
-	});
+    socket.on('new_user_conected', user => {
+        io.emit('user_conected', user);
+    });
 });
 
 http.listen(3000, () => {
-	console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
