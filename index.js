822d7755d652b9cff642d4fd3a0dc64366f07cd0
Manuel Zambrano <mmzmm3z@gmail.com>
2017-01-18 21:00:51 -0300

    Agregada dependencia de socket.io

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const app = require('express')();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-	res.send('<h1>Hello World</H1>');
+	res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', socket => {
+	console.log('a user connected');
+})
 
 http.listen(3000, () => {
 	console.log('listening on *:3000');
